e43f930464f8b5ab4b65d3fd4217e8bb252551e9
Neeraj <neeraj@avantari.org>
2024-11-07 13:37:38 +0530

    ui and locall test

--- app/src/main/java/com/example/composeusingstate/MainActivity.kt
+++ app/src/main/java/com/example/composeusingstate/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -174,7 +175,8 @@ fun RoundTheTip(
  * Example would be $10.00
  */
 
-private  fun calculateTip(amount:Double,tipPercent:Double = 15.0,roundUp:Boolean):String{
+@VisibleForTesting
+internal fun calculateTip(amount:Double,tipPercent:Double = 15.0,roundUp:Boolean):String{
     var tip = tipPercent/100 * amount
     if(roundUp){
         tip = ceil(tip)
---
+++ app/src/test/java/com/example/composeusingstate/TipCalculatorTest.kt
@@ -0,0 +1,17 @@
+package com.example.composeusingstate
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.text.NumberFormat
+
+class TipCalculatorTest {
+
+    @Test
+    fun calculateTip_20PercentNoRoundUp(){
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount,tipPercent,false)
+        assertEquals(expectedTip,actualTip)
+    }
+}
